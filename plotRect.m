3766ccb93cb5d42451c51945a45a1d1f34996a5c
HeshamMeneisi <heshammeneisi@gmail.com>
2015-03-24 01:33:07 +0200

    Update plotRect.m

--- plotRect.m
+++ plotRect.m
@@ -1,6 +1,6 @@
 function [t,x] = plotRect(t,A,Shift,width)
 start = Shift - (width/2);
 finish = Shift + (width/2);
-x = A * rectangularPulse(start,finish,t);
-% plot(t,x,'LineWidth',3);
+x = [0 ones(1,length(t)-2)*A 0];
+plot(t,x,'LineWidth',3);
 end

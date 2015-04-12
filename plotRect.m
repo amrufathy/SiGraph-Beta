74e54b862548d8964b599abfd7f6d65d2c13474d
Amr Fathy <amru_fathy@yahoo.com>
2015-04-12 12:17:11 +0200

    Fixed plotting in case of concatenation & shifting

--- plotRect.m
+++ plotRect.m
@@ -1,7 +1,9 @@
 function [t,x] = plotRect(t,A,shift,width)
-middle = t((1+end)/2);
-shift = shift + abs(t(1));
+middle = (t(1)+t(end))/2;
 start = (middle - (width/2)) + shift;
 finish = (middle + (width/2)) + shift;
-x = A * rectangularPulse(start,finish,t);
+% x = A * rectangularPulse(start,finish,t);
+% x = [zeros(1,(length(t)/4)), A * ones(1,(length(t)/2)), zeros(1,(length(t)/4)), 0];
+x = zeros(size(t));
+x(find(t>start & t<finish)) = A;
 end

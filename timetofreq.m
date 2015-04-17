017fa3565900dc10db22608da331a18557b80f3e
Amr Fathy <amru_fathy@yahoo.com>
2015-04-17 21:33:03 +0200

    Adjustments

--- timetofreq.m
+++ timetofreq.m
@@ -1,7 +1,9 @@
 function [F,Y] = timetofreq (T,X)
 n = length(X);
 ts = T(2)-T(1);
+fs = 1/ts;
 Y = abs(fft(X));
-Y = Y(1:n/2).^2;
-F = (1:(n/2))/ts;
+% Y = Y(1:n/2);
+% F = (1:(n/2))*fs;
+F = (1:n)*fs;
 end

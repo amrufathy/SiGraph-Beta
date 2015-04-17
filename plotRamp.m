9d8f49eef1dacd4e835bcaa044a60d58ada18738
Amr Fathy <amru_fathy@yahoo.com>
2015-04-17 20:06:38 +0200

    Fixes in plotting

--- plotRamp.m
+++ plotRamp.m
@@ -1,9 +1,4 @@
 function [t,x] = plotRamp(t,slope,shift)
 shift = shift + t(1);
-if(t(1) >= 0)
-    x = slope*t - shift; 
-else
-    x = slope*t + shift;
-end
-
+x = slope * t + shift;
 end
--- plotSine.m
+++ plotSine.m
@@ -1,3 +1,4 @@
 function [t,x] = plotSine(t,A,f,shift)
+shift = shift * 3.1415926535897932384626433832795 / 180;
 x = A * sin(2*pi*f*t + shift);
 end
